2af67c882519ddc386c672a039502b854fcf3fd8
Pedro Sousa <ppls2106@gmail.com>
2021-01-11 20:50:33 -0300

    adicionando as implementações das questões

--- client.py
+++ client.py
@@ -8,8 +8,28 @@ pessoa = {
     'foto': ''
 }
 
+pessoa1 = {
+    'email': 'email1@gmail.com',
+    'nome': 'Edmilton',
+    'sobrenome': 'Peixeira',
+    'residencia': 'Belém',
+    'foto': ''
+}
+
+pessoa2 = {
+    'email': 'jose@gmail.com',
+    'nome': 'José',
+    'sobrenome': 'Jesus',
+    'residencia': 'Marituba',
+    'foto': ''
+}
+
 formacao = {
     'nome': 'Sistemas de Informacao',
+}
+
+formacao1 = {
+    'nome': 'Ciencias da Computacao',
 }
 
 habilidade = {
@@ -30,9 +50,22 @@ experiencia1 = {
 
 perfil_maker = Pyro5.api.Proxy('PYRONAME:perfis')
 perfil_maker.open_session()
-print(perfil_maker.cadastrar_perfil(experiencia, formacao, habilidade, pessoa))
-print(perfil_maker.cadastrar_perfil(experiencia, formacao, habilidade1, pessoa))
-print(perfil_maker.cadastrar_perfil(experiencia1, formacao, habilidade, pessoa))
+(perfil_maker.cadastrar_perfil(experiencia, formacao, habilidade, pessoa))
+(perfil_maker.cadastrar_perfil(experiencia, formacao, habilidade, pessoa1))
+(perfil_maker.cadastrar_perfil(experiencia, formacao, habilidade1, pessoa1))
+(perfil_maker.cadastrar_perfil(experiencia, formacao, habilidade1, pessoa))
+(perfil_maker.cadastrar_perfil(experiencia1, formacao, habilidade, pessoa))
+(perfil_maker.cadastrar_perfil(experiencia1, formacao1, habilidade1, pessoa2))
+print('Q1')
+print(perfil_maker.pessoas_por_formacao(formacao))
+print('Q2')
+print(perfil_maker.pessoas_por_cidade('Belém'))
+print('Q3')
+print(perfil_maker.adicionar_experiencia(pessoa2, experiencia))
+print('Q4')
+print(perfil_maker.experiencia_por_perfil('jose@gmail.com'))
+print('Q5')
 print(perfil_maker.listar_pessoas())
-perfil_maker.pessoas_por_formacao(formacao)
+print('Q6')
+print(perfil_maker.get_perfil_by_id('email1@gmail.com'))
 perfil_maker.close_session()
--- server.py
+++ server.py
@@ -33,6 +33,9 @@ class Server:
             if pessoa is None:
                 pessoa = Pessoa(**pessoa_dict)
 
+            pessoa.nome = pessoa_dict['nome']
+            pessoa.sobrenome = pessoa_dict['sobrenome']
+            pessoa.residencia = pessoa_dict['residencia']
             pessoa.experiencias.append(experiencia) if experiencia not in pessoa.experiencias else None
             pessoa.formacoes.append(formacao) if formacao not in pessoa.formacoes else None
             pessoa.habilidades.append(habilidade) if habilidade not in pessoa.habilidades else None
@@ -43,14 +46,51 @@ class Server:
             print(e)
 
     def pessoas_por_formacao(self, formacao):
-        # pessoas = DAO.buscar_todos_por_join(session=self.session, table1=Pessoa, table2=Formacao, order_by=Pessoa.nome,
-        #                                     nome=curso)
-        pessoas = self.session.query(Pessoa, Formacao).filter(Formacao.nome == formacao['nome']).all()
-        print(pessoas)
+        query = self.session.query(Formacao).join(Pessoa, Formacao.pessoas).filter(
+            Formacao.nome == formacao['nome']).first()
+        result = None
+        if Formacao is not None:
+            result = '\n'.join([e.__repr__() for e in query.pessoas])
+        return f'PESSOAS DO CURSO {formacao["nome"]}\n{result}'
+
+    def pessoas_por_cidade(self, cidade):
+        query = self.session.query(Pessoa).join(Habilidade, Pessoa.habilidades).filter(
+            Pessoa.residencia == cidade).all()
+        result = None
+        if len(query) > 0:
+            result = '\n'.join([f'Pessoa: {e.nome} {e.sobrenome}, Habilidades: {e.habilidades}' for e in query])
+        return f'PESSOAS DA CIDADE {cidade}\n{result}\n'
+
+    def adicionar_experiencia(self, pessoa_dict, experiencia_dict):
+        pessoa = DAO.buscar_por_criterio(self.session, Pessoa, email=pessoa_dict['email'])
+        if pessoa is None:
+            return 'Pessoa não encontrada'
+        experiencia = DAO.buscar_por_criterio(self.session, Experiencia, **experiencia_dict)
+        if experiencia is None:
+            experiencia = Experiencia(**experiencia_dict)
+        pessoa.experiencias.append(experiencia) if experiencia not in pessoa.experiencias else None
+        DAO.transacao(self.session, pessoa)
+        return f'ADICIONAR EXPERIENCIA\nExperiencia {experiencia.nome} adicionada a {pessoa.nome} {pessoa.sobrenome}\n'
+
+    def experiencia_por_perfil(self, email):
+        query = self.session.query(Pessoa).join(Experiencia, Pessoa.experiencias).filter(Pessoa.email == email).first()
+        if query is None:
+            return 'Perfil não encontrado'
+        return f'EXPERIENCIA DE {query.nome} {query.sobrenome}\n' \
+               f'{query.experiencias}\n'
 
     def listar_pessoas(self):
         pessoas = DAO.buscar_todos(self.session, Pessoa)
-        return f'Listagem de Pessoas:\n{pessoas}\n'
+        result = None
+        if len(pessoas) > 0:
+            result = '\n'.join([e.__repr__() for e in pessoas])
+        return f'LISTAGEM DE PERFIS:\n{result}\n'
+
+    def get_perfil_by_id(self, email):
+        pessoa = self.session.query(Pessoa).filter(Pessoa.email == email).first()
+        if pessoa is None:
+            return 'Perfil não encontrado'
+        return f'PERFIL COM EMAIL {email}\n{pessoa}'
 
     def open_session(self):
         self.session = Session()
